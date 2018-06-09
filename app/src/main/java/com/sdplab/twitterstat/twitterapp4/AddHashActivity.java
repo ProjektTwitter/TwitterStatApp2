f74da5fbb29ec3769a47a2eb677a347795ea2300
tkrystkowiak <tokrystkowiak@gmail.com>
2018-06-09 12:09:07 +0200

    Improved adding tags add ability to delete them

--- app/src/main/java/com/sdplab/twitterstat/twitterapp4/AddHashActivity.java
+++ app/src/main/java/com/sdplab/twitterstat/twitterapp4/AddHashActivity.java
@@ -3,22 +3,28 @@ package com.sdplab.twitterstat.twitterapp4;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
+
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 
 public class AddHashActivity extends AppCompatActivity implements View.OnClickListener {
 
     private HashtagContainer hc;
     private EditText etOne;
+    private LinearLayout ll;
+    private ArrayList<Button> blist;
+    private ArrayList<LinearLayout> llist;
+    private ArrayList<TextView> tlist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +32,12 @@ public class AddHashActivity extends AppCompatActivity implements View.OnClickLi
         setContentView(R.layout.activity_add_hash);
         Button one = findViewById(R.id.button_add);
         one.setOnClickListener(this);
-
         etOne = findViewById(R.id.editText1);
         hc = HashtagContainer.getInstance();
+        ll = findViewById(R.id.linearLayout);
+        blist = new ArrayList<>();
+        tlist = new ArrayList<>();
+        llist = new ArrayList<>();
         refresh();
     }
 
@@ -42,34 +51,52 @@ public class AddHashActivity extends AppCompatActivity implements View.OnClickLi
                 CharSequence text = "Hashtag added successfuly";
                 int duration = Toast.LENGTH_SHORT;
                 Toast.makeText(context, text, duration).show();
-                etOne.setText("");
+                etOne.setText("#");
                 refresh();
                 break;
 
         }
     }
 
+
     private void refresh(){
-        LinearLayout ll = findViewById(R.id.linearLayout);
-        ArrayList<Button> blist = new ArrayList<>();
+        for(Button b :blist){
+            b.setVisibility(View.GONE);
+        }
+        blist.clear();
+        for(TextView t :tlist){
+            t.setVisibility(View.GONE);
+        }
+        tlist.clear();
+        for(LinearLayout l :llist){
+            l.setVisibility(View.GONE);
+        }
+        llist.clear();
         for(int i = 0; i < hc.getTagList().size(); i++){
-            for(Button b :blist){
-                b.setVisibility(View.GONE);
-            }
             LinearLayout l = new LinearLayout(this);
             l.setId(i);
+            l.setWeightSum(1);
             l.setOrientation(LinearLayout.HORIZONTAL);
             l.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT));
             TextView tv = new TextView(this);
             Button b = new Button(this);
             tv.setText(hc.getTagList().get(i));
             tv.setId(i);
-            tv.setGravity(LinearLayout.FOCUS_LEFT);
+            tv.setTextSize(TypedValue.COMPLEX_UNIT_SP,18f);
+            tv.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT,1f));
             b.setText("X");
             b.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-            b.setGravity(LinearLayout.FOCUS_RIGHT);
             b.setId(i);
+            b.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                   hc.getTagList().remove(v.getId());
+                   refresh();
+                }
+            });
             blist.add(b);
+            llist.add(l);
+            tlist.add(tv);
             l.addView(tv);
             l.addView(b);
             ll.addView(l);
--- app/src/main/java/com/sdplab/twitterstat/utils/Constants.java
+++ app/src/main/java/com/sdplab/twitterstat/utils/Constants.java
@@ -2,9 +2,9 @@ package com.sdplab.twitterstat.utils;
 
 public class Constants {
 
-    public final static String consumerKey = "*";
-    public final static String consumerSecret = "*";
-    public final static String accessToken = "*";
-    public final static String accessSecret = "*";
+    public final static String consumerKey = "0NRMt0oG83AyOKFPh37ii8xk6";
+    public final static String consumerSecret = "jrv9os8dkCeSLvheGeE7mdRZ5V5cLviCBHWlFy3zyri7ejcVIE";
+    public final static String accessToken = "4364663123-X99BG94lfbNdWrZpYIESU1l9TKJOYgv01EFkrwG";
+    public final static String accessSecret = "Dh5v32dFPSCgk8AMxnEUQw4blrknEzdVROlQwt8ICii7f";
 
 }
