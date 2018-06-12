0f31f9cc3ba5e340c0243b5f25aa37bd99b415fd
pancek email = nowackiads@gmail.com <tokrystkowiak@gmail.com>
2018-06-12 22:31:36 +0200

    Test commit

--- app/src/main/java/com/sdplab/twitterstat/twitterapp4/StatisticsActivity.java
+++ app/src/main/java/com/sdplab/twitterstat/twitterapp4/StatisticsActivity.java
@@ -109,7 +109,7 @@ public class StatisticsActivity extends AppCompatActivity {
             }
             BarData bd = new BarData (entriesDataSet);
             barchart.setData(bd);
-            //test2
+            //test3
         }
 
     }
